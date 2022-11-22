7837c975b9ed64eb135771f1736ddbada8c02f28
chriskontsis <cak0927@gmail.com>
2022-11-22 16:02:32 -0700

    making inputData member initialization for better performance

--- inputData.cpp
+++ inputData.cpp
@@ -2,14 +2,8 @@
 #include <string> 
 
 Order::Order(long long time_stamp, std::string client_name, std::string ticker_symbol, long long _price, long long _quantity, 
-             long long _expiration) 
-{
-    timeStamp = time_stamp;
-    clientName = client_name;
-    tickerSymbol = ticker_symbol;
-    price = _price;
-    quantity = _quantity;
-    expiration = _expiration;
-}
+             long long _expiration): timeStamp(time_stamp), clientName(client_name), tickerSymbol(ticker_symbol), price(_price), 
+             quantity(_quantity), expiration(_expiration)
+{}
 
 long long Order::ID = 0;
